fe2707c41d4ce6a6ee8a10430f8820df4c7fb4d5
giseares <64367281+giseares@users.noreply.github.com>
2024-11-12 10:34:49 -0300

    se arreglo la hora, para que tenga formato 24hs y se agrego icon

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateTimes() {
+  const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
+
+  const argentinaTime = new Date(); 
+  document.getElementById("argentina").innerText = `Buenos Aires: ${argentinaTime.toLocaleTimeString('es-AR', options)}`;
+
+  const seattleTime = new Date(argentinaTime);
+  seattleTime.setHours(argentinaTime.getHours() - 5);
+  document.getElementById("seattle").innerText = `Seattle: ${seattleTime.toLocaleTimeString('es-AR', options)}`;
+
+  const londonTime = new Date(argentinaTime);
+  londonTime.setHours(argentinaTime.getHours() + 3);
+  document.getElementById("londres").innerText = `Londres: ${londonTime.toLocaleTimeString('es-AR', options)}`;
+
+  const nigeriaTime = new Date(argentinaTime);
+  nigeriaTime.setHours(argentinaTime.getHours() + 4);
+  document.getElementById("nigeria").innerText = `Nigeria: ${nigeriaTime.toLocaleTimeString('es-AR', options)}`;
+}
+
+updateTimes();
+setInterval(updateTimes, 60000); // Actualiza cada minuto
